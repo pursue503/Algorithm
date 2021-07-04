0729d5789c69c36837f042c618ca19841d589427
pursue503 <pursue503@naver.com>
2021-07-04 21:24:13 +0900

    Level1 키패드

--- src/level01/Programmers67256.java
+++ src/level01/Programmers67256.java
@@ -1,66 +1,85 @@
 package level01;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-
+/**
+ * 
+ * 프로그래머스 레벨1 키패드
+ * 
+ */
 public class Programmers67256 {
 
     public static void main(String[] args) {
 
-        String[][] phoneArr = {
-                {"1","2","3"},
-                {"4","5","6"},
-                {"7","8","9"},
-                {"*","0","#"}
-        };
+        Programmers67256 programmers67256 = new Programmers67256();
+        System.out.println(programmers67256.solution(new int[]{1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5}, "right"));
+    }
 
-        for(int i=0; i<phoneArr.length; i++){
-            for(int j=0; j<phoneArr[i].length; j++) {
-                System.out.print(phoneArr[i][j]);
-                System.out.print(" ");
+    public String solution(int[] numbers, String hand) {
+        StringBuilder answer = new StringBuilder();
+
+        int left = 42;
+        int right = 35;
+
+        for(int i=0; i<numbers.length; i++) {
+            int a = numbers[i];
+            if(numbers[i] == 1 || numbers[i] == 4 || numbers[i] == 7 || left == numbers[i]) {
+                left = numbers[i];
+                answer.append("L");
+            } else if(numbers[i] == 3 || numbers[i] == 6 || numbers[i] == 9 || right == numbers[i]) {
+                right = numbers[i];
+                answer.append("R");
+            } else {
+                if(distance(left,right,numbers[i], hand)) {
+                    right = numbers[i];
+                    answer.append("R");
+                } else {
+                    left = numbers[i];
+                    answer.append("L");
+                }
             }
-            System.out.println();
         }
-
+        return answer.toString();
 
     }
 
-    public static String solution(int[] numbers, String hand) {
+    private boolean distance (int left, int right, int num , String hand) { // right 가까우면 ture // left 가 가까우면 false
 
-        String[][] phoneArr = {
-                {"1","2","3"},
-                {"4","5","6"},
-                {"7","8","9"},
-                {"*","0","#"}
+        int[] leftDistance = new int[2];
+        int[] rightDistance = new int[2];
+        int[] numArr = new int[2];
+
+        int[][] key = new int[][]{
+                {1,2,3},
+                {4,5,6},
+                {7,8,9},
+                {42,0,35}
         };
 
-
-        StringBuilder builder = new StringBuilder();
-
-        String left = "*";
-
-        String right = "#";
-
-        for(int i=0; i<numbers.length; i++) {
-            // 1 4 7 은 왼손으로만 누른다
-
-            if(numbers[i] == 1 || numbers[i] == 4 || numbers[i] == 7) {
-                builder.append("L");
-                left = String.valueOf(numbers[i]);
-                continue;
-            } else if(numbers[i] == 1 || numbers[i] == 4 || numbers[i] == 7) {
-                builder.append("R");
-                right = String.valueOf(numbers[i]);
-                continue;
+        for(int i=0; i<key.length; i++) {
+            for(int j=0; j<key[j].length; j++) {
+                if(left == key[i][j]) {
+                    leftDistance[0] = i;
+                    leftDistance[1] = j;
+                }
+                if(right == key[i][j]) {
+                    rightDistance[0] = i;
+                    rightDistance[1] = j;
+                }
+                if(num == key[i][j]) {
+                    numArr[0] = i;
+                    numArr[1] = j;
+                }
             }
-
-            // 나머지 2 5 8 0
-
-
-
         }
-
-        return null;
+        int leftNum = Math.abs(numArr[0] - leftDistance[0]) + Math.abs(numArr[1] - leftDistance[1]);
+        int rightNum = Math.abs(numArr[0] - rightDistance[0]) + Math.abs(numArr[1] - rightDistance[1]);
+        if(leftNum == rightNum) {
+            if(hand.equals("right")) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+        return rightNum < leftNum;
     }
 
 }
