f329d511a4943480c05e160f1a0cb3e4c6947bc5
pursue503 <pursue503@naver.com>
2021-07-26 21:58:48 +0900

    Level1 체육복 다시 품

--- src/level01/Programmers42862.java
+++ src/level01/Programmers42862.java
@@ -2,6 +2,8 @@ package level01;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.stream.Collectors;
 
 /**
@@ -11,6 +13,14 @@ import java.util.stream.Collectors;
  */
 
 public class Programmers42862 {
+
+    public static void main(String[] args) {
+        Programmers42862 programmers42862 = new Programmers42862();
+//        System.out.println(programmers42862.solution2(5,new int[]{2,4}, new int[]{3}));
+//        System.out.println(programmers42862.solution2(5,new int[]{2, 3, 4}, new int[]{1, 2, 3}));
+        System.out.println(programmers42862.solution2(5,new int[]{2, 3, 4}, new int[]{3 , 4 , 5}));
+//        System.out.println(programmers42862.solution2(5,new int[]{3, 4}, new int[]{4,5}));
+    }
 
     public int solution(int n, int[] lost, int[] reserve) {
         int answer = 0;
@@ -54,6 +64,47 @@ public class Programmers42862 {
         }
 
 
+        return answer;
+    }
+
+    public int solution2(int n, int[] lost, int[] reserve) {
+        int answer = n - lost.length;
+
+        ArrayList<Integer> lostList = new ArrayList<>();
+        Queue<Integer> queue = new LinkedList<>();
+
+        for(int i=0; i<lost.length; i++) {
+            lostList.add(lost[i]);
+        }
+
+        for(int i=0; i<reserve.length; i++) {
+            int num = lostList.indexOf(reserve[i]);
+            if(num != -1) {
+                lostList.remove(num);
+                answer++;
+                continue;
+            }
+            queue.add(reserve[i]);
+        }
+
+        while (!queue.isEmpty()) {
+            int x = queue.poll();
+
+            int rm = lostList.indexOf(x - 1);
+
+            if(rm != -1) {
+                lostList.remove(rm);
+                answer++;
+                continue;
+            }
+
+            int rm2 = lostList.indexOf(x + 1);
+            if(rm2 != -1) {
+                lostList.remove(rm2);
+                answer++;
+            }
+
+        }
         return answer;
     }
 
