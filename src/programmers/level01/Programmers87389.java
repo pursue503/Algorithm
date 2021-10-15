2c5c7c3947f2c0c3c2c16d757190d324eb22486e
pursue503 <pursue503@naver.com>
2021-10-15 23:19:07 +0900

    Add Level1 나머지가 1이 되는 수 찾기

---
+++ src/programmers/level01/Programmers87389.java
@@ -0,0 +1,18 @@
+package programmers.level01;
+
+/**
+ *
+ * 나머지가 1이 되는 수 찾기
+ *
+ */
+public class Programmers87389 {
+
+    public int solution(int n) {
+        for(int i=2; i<n; i++) {
+            if(n % i == 1) {
+                return i;
+            }
+        }
+        return 0;
+    }
+}
