0f33946aaaae0c50ce9512ec2347c9e9e2a198d6
pursue503 <pursue503@naver.com>
2022-01-04 21:56:07 +0900

    LeetCode 1009

---
+++ src/leetcode/study/day1/LeetCode1009.java
@@ -0,0 +1,33 @@
+package leetcode.study.day1;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ *
+ * Complement of Base 10 Integer
+ * https://leetcode.com/problems/complement-of-base-10-integer/
+ */
+
+public class LeetCode1009 {
+
+    public static void main(String[] args) {
+        System.out.println(new LeetCode1009().bitwiseComplement(10));
+    }
+
+    public int bitwiseComplement(int n) {
+        return Integer.parseInt(toReversBinary(toBinary(n)), 2);
+    }
+
+    public String toBinary(int number) {
+        return Integer.toBinaryString(number);
+    }
+
+    public String toReversBinary(String binary) {
+        StringBuilder builder = new StringBuilder();
+        binary.chars().map(num -> num == '1' ? 0 : 1).boxed().forEach(builder::append);
+        return builder.toString();
+    }
+
+
+}
