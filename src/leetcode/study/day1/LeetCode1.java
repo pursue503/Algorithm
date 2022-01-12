8611b5045ec828c40429133a1d4c9caf0ee7cea6
pursue503 <pursue503@naver.com>
2022-01-12 17:52:34 +0900

    LeetCode 1 two sum

---
+++ src/leetcode/study/day1/LeetCode1.java
@@ -0,0 +1,58 @@
+package leetcode.study.day1;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * two sum
+ * https://leetcode.com/problems/two-sum/
+ */
+
+public class LeetCode1 {
+
+    public int[] twoSum2(int[] nums, int target) {
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+
+        return new int[]{};
+    }
+
+//    public int[] twoSum(int[] nums, int target) {
+//
+//        int[] answer = new int[2];
+//
+//        Map<Integer, Integer> map = new HashMap<>();
+//
+//        for (int i = 0; i < nums.length; i++) {
+//            map.put(nums[i], i);
+//        }
+//
+//
+//        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+//
+//            int findKey = target - entry.getKey();
+//
+//            if (map.containsKey(findKey)) {
+//                answer[0] = entry.getValue();
+//                answer[1] = map.get(findKey);
+//
+//                return answer;
+//            }
+//
+//        }
+//
+//        return answer;
+//    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(new LeetCode1().twoSum(new int[]{3, 3}, 6)));
+    }
+
+}
